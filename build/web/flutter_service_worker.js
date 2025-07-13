7dab98ee1cd3ac99f63f60df940d9610d946d70e
tapendrabistao1 <tapendrabista01@gmail.com>
2025-07-13 20:34:41 +0545

    8-34

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -83,7 +83,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "152e5f6d3a9baf8b7f0ad9ca29c31dc9
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "b8de4d2a472714a196a189fc8ba8abf9",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "1fcd66a4566a1f77f25b72d4ec015c3a",
+"flutter_bootstrap.js": "7772386514872afa2a65657003a1ad5e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
